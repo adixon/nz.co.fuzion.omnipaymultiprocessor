0327043f2ff462f52c0e1ea30ef1693c49c30978
adixon <alan.g.dixon@gmail.com>
2016-09-23 16:07:52 -0400

    added bluepay as per readme

--- CRM/Core/Payment/processors.mgd.php
+++ CRM/Core/Payment/processors.mgd.php
@@ -320,8 +320,7 @@ return array(
       'is_recur' => 1,
     ),
   ),
-  12 =>
-    array(
+  12 => array(
       'name' => 'OmniPay - PayPal_Standard',
       'entity' => 'payment_processor_type',
       'params' =>
@@ -426,4 +425,28 @@ return array(
       'supports_preapproval' => 1,
     ),
   ),
+  17 => array(
+    'name' => 'OmniPay - BluePay',
+    'entity' => 'payment_processor_type',
+    'params' => array(
+      'version' => 3,
+      'title' => 'OmniPay - BluePay',
+      'name' => 'omnipay_BluePay',
+      'description' => 'Omnipay BluePay',
+      // DO NOT CHANGE: Labels are used as Omnipay gateway properties.
+      'user_name_label' => 'Api Key',
+      'password_label' => 'Password',
+      'class_name' => 'Payment_OmnipayMultiProcessor',
+      'url_site_default' => 'http://unused.com',
+      'url_api_default' => 'http://unused.com',
+      'url_recur_default' => 'http://unused.com',
+      'url_site_test_default' => 'http://unused.com',
+      'url_recur_test_default' => 'http://unused.com',
+      'url_api_test_default' => 'http://unused.com',
+      'billing_mode' => 1,
+      'payment_type' => 1,
+      'is_recur' => 1,
+    ),
+  ),
+
 );
